63006ab829497dc2edd4dec8330526f73d7bc0f8
ATN04 <95452323+EtienneK2004@users.noreply.github.com>
2024-01-23 17:00:21 +0100

    TP1

--- src/blockchainStorage.js
+++ src/blockchainStorage.js
@@ -2,10 +2,12 @@ import {readFile, writeFile} from 'node:fs/promises'
 import {getDate, monSecret} from "./divers.js";
 import {NotFoundError} from "./errors.js";
 import {createHash} from 'node:crypto'
+import {v4 as uuidv4} from 'uuid'
+
 
 
 /* Chemin de stockage des blocks */
-const path = ''
+const path = './data/blockchain.json'
 
 /**
  * Mes définitions
@@ -23,7 +25,9 @@ const path = ''
  * @return {Promise<any>}
  */
 export async function findBlocks() {
-    // A coder
+    const fileContent = await readFile(path, 'utf-8');
+    const jsonData = JSON.parse(fileContent);
+    return jsonData;
 }
 
 /**
@@ -31,8 +35,10 @@ export async function findBlocks() {
  * @param partialBlock
  * @return {Promise<Block[]>}
  */
-export async function findBlock(partialBlock) {
-    // A coder
+export async function findBlock(id) {
+    const data = await findBlocks();
+    const block = data.find(block => block.id === id);
+    return block;
 }
 
 /**
@@ -40,7 +46,8 @@ export async function findBlock(partialBlock) {
  * @return {Promise<Block|null>}
  */
 export async function findLastBlock() {
-    // A coder
+    const data = await findBlocks();
+    return data[data.length - 1];
 }
 
 /**
@@ -49,6 +56,29 @@ export async function findLastBlock() {
  * @return {Promise<Block[]>}
  */
 export async function createBlock(contenu) {
-    // A coder
+    const id = uuidv4();
+    const date = getDate();
+    const last = await findLastBlock() || monSecret;
+    const hash = createHash('sha256').update(last + date + contenu.nom + contenu.don).digest('hex');
+    const newBlock = {id, date, hash, ...contenu};
+    const blocks = await findBlocks();
+
+
+    blocks.push(newBlock);
+    writeFile(path, JSON.stringify(blocks));
+    return [newBlock];
 }
 
+export async function verifBlocks() {
+    const blocks = await findBlocks();
+    let lastHash = monSecret; //Le premier hash est le hash du secret
+    for (let block of blocks) {
+    //On vérifie que le has obtenu avec le block et le lastHash est bien le hash du block
+        if (block.hash !== createHash('sha256').update(lastHash + block.date + block.nom + block.don).digest('hex')) {
+            return false;
+        }
+        lastHash = block.hash;
+    }
+    //Aucun problème, on renvoie true
+    return true;
+}
